0278356fa1a9fd9f8f0cf2cf831ff1967955792a
bdoiron98 <bdoiro4@lsu.edu>
2020-03-31 21:30:10 -0500

    tool template off

--- django_pages/settings.py
+++ django_pages/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages' # new
+    'users', # new
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser' # new
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
